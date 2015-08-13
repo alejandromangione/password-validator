09df699b562e0c09b063d721fc787a017c9379ba
Alejandro Mangione <alejandro.ferraiolo@brasilct.com>
2015-08-13 13:40:09 -0300

    Source file updated

--- src/passwordValidator.js
+++ src/passwordValidator.js
@@ -4,18 +4,14 @@ passwordRules._min = 6;
 passwordRules._max = 20;
 passwordRules._sequences = [ ["123456789"], ["abcdefghijklmnopqrstuvwxyz"] ];
 
+
 passwordRules.checkCharacters = function(str) {
-    //
-    if( (new RegExp("\\W")).test(str) && (new RegExp("\\w")).test(str) && !(new RegExp("\\ ")).test(str)) {
-      return true;
-    }
-
-    return false;
-  }
-
-passwordRules.checkSize = function(str) {
-
-  if(str.length >= passwordRules._min && str.length <= passwordRules._max) {
+  //
+  if(
+    !(new RegExp("\\ ")).test(str) &&
+    (new RegExp(/^(?=.*\d)(?=.*[a-zA-Z])(?=.*[\W\_]).{6,20}$/)).test(str)
+    )
+  {
     return true;
   }
 
@@ -24,29 +20,29 @@ passwordRules.checkSize = function(str) {
 
 passwordRules.checkSequence = function(str) {
 
-    for (var i=0; i < str.length - 2; i++){
-      var pwdPart = str.substring(i, i+3);
-      var patt = new RegExp(pwdPart);
+  for (var i=0; i < str.length - 2; i++){
+    var pwdPart = str.substring(i, i+3);
+    var patt = new RegExp(pwdPart);
 
-      for (s in passwordRules._sequences) {
+    for (s in passwordRules._sequences) {
 
-        for (var k=0; k < passwordRules._sequences.length; k++){
-          if (patt.test(passwordRules._sequences[k])) {
-            return false;
-          }
+      for (var k=0; k < passwordRules._sequences.length; k++){
+        if (patt.test(passwordRules._sequences[k])) {
+          return false;
         }
       }
 
-
     }
 
-    return true;
+  }
+
+  return true;
 
 }
 
 passwordRules.validate = function(str) {
 
-  var result = passwordRules.checkCharacters(str) && passwordRules.checkSize(str) && passwordRules.checkSequence(str);
+  var result = passwordRules.checkCharacters(str) && passwordRules.checkSequence(str);
 
   return result;
 
